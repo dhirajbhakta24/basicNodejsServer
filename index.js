98641ef7b1cabcf205d273747a2aa0e77d7d63bb
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-05-03 00:25:40 +0530

    setup up basic http nodejs server

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 //setting up a basic nodejs server 
-console.log("nodejsServer");
+//http module can help you to create http server
+
+const http = require("http");
+const PORT = 3000;
+
+const server = http.createServer(async (req,res)=>{
+    console.log("req  received ");
+    if(req.method =="GET"){
+        res.end("GET  request received");
+    }
+    else if(req.method =="POST"){
+        res.writeHead(201);
+        res.end("POST request received");
+    }
+    else {
+        res.end("Hello world ");
+    }
+});
+
+server.listen(PORT,()=>{
+    console.log(`server started at port ${PORT}`);
+});
